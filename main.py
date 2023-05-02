c3b0b253fa7f33a1f9eae79c17179fe532f7e3ce
frido2 <frido_petersen@web.de>
2023-05-02 15:54:30 +0200

    testing_out

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # hier werden alle inputs zusammgengefügt.
-import packages
+# import packages
 
 # bitte alle funktionen, die verwendet werden sollen einzeln so importieren, nicht ganze dateien importieren
-from test_func import fib, primzahlen
+from functions import fib, primzahlen
 
 if __name__ == '__main__':
     print(fib(12))
 
     # die ersten x primzahlen
     print(primzahlen(100))
+# ich teste ob es funktionuckelt
+    print("dario ist schwer beeindruckt")
