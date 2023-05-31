3cc3646787d7697fea9e15690a29e40f2e588889
maxfidi <maxfidi@gmx.de>
2023-05-31 13:48:27 +0200

    minor changes

--- data_exploration/statistical_functions.py
+++ data_exploration/statistical_functions.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import data_cleanup as dc
+
 def p_cor():
     # TODO implement correlation
     pass
@@ -5,3 +8,12 @@ def p_cor():
 def s_cor():
     # TODO implement correlation
     pass
+def distance_matrix(frame: pd.DataFrame) -> pd.DataFrame:
+    """aus einem Datensatz, den wir gegeben haben, direkt eine Distanzmatrix zu erstellen. Zeilen in dem ausgegebenen
+    DataFrame sind die alten AS, Spalten die neuen"""
+    mutations_df = dc.aufteilung_mut_pos(frame)
+    subs_df = mutations_df.groupby(["AS_old", "AS_new"])
+    mean_scores = subs_df.DMS_score.mean()
+    mean_scores_df = mean_scores.reset_index()
+    frame = mean_scores_df.pivot(index="AS_old", columns="AS_new", values="DMS_score")
+    return frame
