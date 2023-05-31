6da90e77877de15614eeeda12dff2893a29df0ec
ennoschaefer <enno.schaefer@stud.uni-heidelberg.de>
2023-05-31 14:19:19 +0200

    creating distance matrix
    added a explanation in the notebook,
    tried to add the function for data exploration

--- data_exploration/statistical_functions.py
+++ data_exploration/statistical_functions.py
@@ -5,3 +5,11 @@ def p_cor():
 def s_cor():
     # TODO implement correlation
     pass
+def distance_matrix(frame: pd.DataFrame) -> pd.DataFrame:
+    """..."""
+    mutations_df = dc.aufteilung_mut_pos(frame)
+    subs_df = mutations_df.groupby(["AS_old", "AS_new"])
+    mean_scores = subs_df.DMS_score.mean()
+    mean_scores_df = mean_scores.reset_index()
+    frame = mean_scores_df.pivot(index="AS_old", columns="AS_new", values="DMS_score")
+    return frame
