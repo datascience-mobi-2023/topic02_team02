181cceb9b4430ac38bf62ebcddef456e8ae5b216
ennoschaefer <enno.schaefer@stud.uni-heidelberg.de>
2023-05-31 14:26:50 +0200

    added distance matrix function

--- data_exploration/statistical_functions.py
+++ data_exploration/statistical_functions.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import data_cleanup as dc
+
 def p_cor():
     # TODO implement correlation
     pass
@@ -6,7 +9,7 @@ def s_cor():
     # TODO implement correlation
     pass
 def distance_matrix(frame: pd.DataFrame) -> pd.DataFrame:
-    """..."""
+    """aus einem Datensatz, den wir gegeben haben, direkt eine Distanzmatrix zu erstellen. Zeilen sind die alten AS, Spalten die neuen"""
     mutations_df = dc.aufteilung_mut_pos(frame)
     subs_df = mutations_df.groupby(["AS_old", "AS_new"])
     mean_scores = subs_df.DMS_score.mean()
