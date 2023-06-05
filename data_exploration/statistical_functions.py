27b889786a556ebfc0034558cc0905f95b30cc9f
darioandrea <dario.prifti@stud.uni-heidelberg.de>
2023-06-05 13:34:56 +0200

    neue invertierte Distanzmatrix funktion + autoreload wegen komischem Fehler

--- data_exploration/statistical_functions.py
+++ data_exploration/statistical_functions.py
@@ -17,3 +17,14 @@ def distance_matrix(frame: pd.DataFrame) -> pd.DataFrame:
     mean_scores_df = mean_scores.reset_index()
     frame = mean_scores_df.pivot(index="AS_old", columns="AS_new", values="DMS_score")
     return frame
+
+def distance_matrix_inverted(frame: pd.DataFrame) -> pd.DataFrame:
+    """aus einem Datensatz, den wir gegeben haben, direkt eine Distanzmatrix zu erstellen. Zeilen in dem ausgegebenen
+    DataFrame sind die alten AS, Spalten die neuen"""
+    mutations_dfi = dc.aufteilung_mut_pos(frame)
+    subs_dfi = mutations_dfi.groupby(["AS_old", "AS_new"])
+    mean_scores = subs_dfi.DMS_score.mean()
+    mean_scores_dfi = mean_scores.reset_index()
+    frame = mean_scores_dfi.pivot(index="AS_new", columns="AS_old", values="DMS_score")
+    return frame
+
