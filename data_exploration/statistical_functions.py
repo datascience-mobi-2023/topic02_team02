e17f380aaadcca7873f98b704c23335b3548a056
ennoschaefer <enno.schaefer@stud.uni-heidelberg.de>
2023-06-13 17:33:38 +0200

    Update: bisherige Distanzmatrix als mean_substitutions umbenannt

--- data_exploration/statistical_functions.py
+++ data_exploration/statistical_functions.py
@@ -1,14 +1,8 @@
 import pandas as pd
 import data_cleanup as dc
 
-def p_cor():
-    # TODO implement correlation
-    pass
 
-def s_cor():
-    # TODO implement correlation
-    pass
-def distance_matrix(frame: pd.DataFrame) -> pd.DataFrame:
+def mean_substitutions(frame: pd.DataFrame) -> pd.DataFrame:
     """aus einem Datensatz, den wir gegeben haben, direkt eine Distanzmatrix zu erstellen. Zeilen in dem ausgegebenen
     DataFrame sind die alten AS, Spalten die neuen"""
     mutations_df = dc.aufteilung_mut_pos(frame)
@@ -18,13 +12,12 @@ def distance_matrix(frame: pd.DataFrame) -> pd.DataFrame:
     frame = mean_scores_df.pivot(index="AS_old", columns="AS_new", values="DMS_score")
     return frame
 
-def distance_matrix_inverted(frame: pd.DataFrame) -> pd.DataFrame:
+def mean_substitutions_inverted(frame: pd.DataFrame) -> pd.DataFrame:
     """aus einem Datensatz, den wir gegeben haben, direkt eine Distanzmatrix zu erstellen. Zeilen in dem ausgegebenen
-    DataFrame sind die alten AS, Spalten die neuen"""
+    DataFrame sind die neuen AS, Spalten die alten"""
     mutations_dfi = dc.aufteilung_mut_pos(frame)
     subs_dfi = mutations_dfi.groupby(["AS_old", "AS_new"])
     mean_scores = subs_dfi.DMS_score.mean()
     mean_scores_dfi = mean_scores.reset_index()
     frame = mean_scores_dfi.pivot(index="AS_new", columns="AS_old", values="DMS_score")
     return frame
-
