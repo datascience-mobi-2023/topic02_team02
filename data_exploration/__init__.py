d67ba3f0820141c642a1acc1d8f9026784b760a3
ennoschaefer <enno.schaefer@stud.uni-heidelberg.de>
2023-06-16 10:58:09 +0200

    Turned AA-dataset cleaning etc. into a function called aa_distance_matrix. Added some suggestions for Frido as well

--- data_exploration/__init__.py
+++ data_exploration/__init__.py
@@ -2,5 +2,6 @@ from data_exploration.statistical_functions import *
 
 __all__ = [
     'mean_substitutions_inverted',
+    "aa_distance_matrix",
     "mean_substitutions"
 ]
--- data_exploration/statistical_functions.py
+++ data_exploration/statistical_functions.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import data_cleanup as dc
-
+from sklearn.metrics.pairwise import euclidean_distances
+import scipy.stats as stats
+import data_exploration as de
 
 def mean_substitutions(frame: pd.DataFrame) -> pd.DataFrame:
     """aus einem Datensatz, den wir gegeben haben, direkt eine Distanzmatrix zu erstellen. Zeilen in dem ausgegebenen
@@ -21,3 +23,14 @@ def mean_substitutions_inverted(frame: pd.DataFrame) -> pd.DataFrame:
     mean_scores_dfi = mean_scores.reset_index()
     frame = mean_scores_dfi.pivot(index="AS_new", columns="AS_old", values="DMS_score")
     return frame
+
+def aa_distance_matrix(frame: pd.DataFrame) -> pd.DataFrame:
+    """  """
+    aa_nat = frame.drop(index=[12, 18])
+    labels_column = 'Letter'
+    aa_rmv = aa_nat.drop(['Name', 'Abbr', 'Letter', 'Molecular Formula', 'Molecular Weight', 'Residue Formula', 'pKx3'], axis=1)
+    aa_zscore = aa_rmv.apply(stats.zscore)
+    aa_distances = euclidean_distances(aa_zscore.values)
+    frame = pd.DataFrame(aa_distances, index=aa_nat[labels_column], columns=aa_nat[labels_column])
+    return frame
+
