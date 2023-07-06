1ee0d74a85937ff5d5a2ac85db7c2d88dfdf3146
ennoschaefer <enno.schaefer@stud.uni-heidelberg.de>
2023-07-06 10:34:05 +0200

    final additions for de. implemented distance_matrix_wt and mutated

--- data_exploration/__init__.py
+++ data_exploration/__init__.py
@@ -4,7 +4,8 @@ __all__ = [
     "mean_substitutions",
     'mean_substitutions_inverted',
     "aa_distance_matrix",
-    "dms_distance_matrix",
+    "dms_distance_matrix_wt",
+    "dms_distance_matrix_mutated",
     "plot_hier_clust",
     "determine_clusters_silhouette",
     "pca_hierarchical_plot"
